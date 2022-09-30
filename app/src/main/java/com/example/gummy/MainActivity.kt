c09a38fd1cb4eadd9a5654bde7e9ddd46e1fdf3e
Jackx201 <zahuldmz@live.com>
2022-09-29 22:55:02 -0500

    Basic Practice

--- app/src/main/java/com/example/gummy/MainActivity.kt
+++ app/src/main/java/com/example/gummy/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.gummy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gummy.ui.theme.GummyTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +36,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun sayHello(textTobeDisplayed: String) {
+    Text(
+        text = textTobeDisplayed,
+        modifier = Modifier
+            .height(200.dp)
+            .width(100.dp)
+            .padding(10.dp)
+            .clickable { })
+}
+
+@Preview(
+    showBackground = true,
+    name = "Test with Oreo",
+    showSystemUi = true,
+    device = Devices.NEXUS_6
+)
+@Composable
+fun HelpSayHello() {
+    sayHello("Hello World")
 }
 
 @Preview(showBackground = true)
