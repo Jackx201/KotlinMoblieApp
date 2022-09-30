3c640370390704c04cce3dcc66d68cf2574ebd2c
Jackx201 <zahuldmz@live.com>
2022-09-29 23:12:33 -0500

    Scroll Added

--- app/src/main/java/com/example/gummy/MainActivity.kt
+++ app/src/main/java/com/example/gummy/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.gummy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,31 +37,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun sayHello(textTobeDisplayed: String) {
-    Text(
-        text = textTobeDisplayed,
-        modifier = Modifier
-            .height(200.dp)
-            .width(100.dp)
-            .padding(10.dp)
-            .clickable { })
+fun myBox() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .fillMaxWidth()) {
+        Box(modifier = Modifier
+            //.width(100.dp)
+            .height(100.dp)
+            .background(color = Color.Magenta)
+            .verticalScroll(rememberScrollState())) {
+            Text(text = "Hello I am a box")
+            Text(text = "Hello I am a box")
+            Text(text = "Hello I am a box")
+            Text(text = "Hello I am a box")
+            Text(text = "Hello I am a box")
+
+        }
+    }
 }
 
-@Preview(
-    showBackground = true,
-    name = "Test with Oreo",
-    showSystemUi = true,
-    device = Devices.NEXUS_6
-)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun HelpSayHello() {
-    sayHello("Hello World")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun DefaultMyBox() {
     GummyTheme {
-        Greeting("Android")
+        myBox()
     }
 }
